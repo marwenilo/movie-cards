763c6e2a7602d69d9ae4a9843e9f8d0294b61e0c
Marwen Magri <marwen.magri@gomycode.co>
2020-01-08 15:43:07 +0100

    first commit and setup the card ske

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './components/Card'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card } from 'antd';
+const { Meta } = Card;
+ const Cardd=()=> {
+    return (
+        <div>
+              <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+        </div>
+    )
+}
+
+export default Cardd
+
+
+
+
+
